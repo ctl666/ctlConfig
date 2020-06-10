87995d228a018eea04f884a6767f7d730f45d3c6
ctl <1223240795@qq.com>
2020-06-10 19:28:56 +0800

    提交代码，client模块

---
+++ springcloud-config-git-client/src/main/java/com/ctl/springcloudconfiggitclient/SpringcloudConfigGitClientApplication.java
@@ -0,0 +1,13 @@
+package com.ctl.springcloudconfiggitclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigGitClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigGitClientApplication.class, args);
+    }
+
+}
---
+++ springcloud-config-git-client/src/main/java/com/ctl/springcloudconfiggitclient/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.ctl.springcloudconfiggitclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/aa")
+    public String getProfile(){
+        return this.hello;
+    }
+
+}
